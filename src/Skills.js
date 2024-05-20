06414bf47d2e803da42098e87595303b11aa8b00
Nawaz Sharief <112409617+nsharie@users.noreply.github.com>
2024-05-20 11:33:52 +0530

    Update Skills.js

--- src/Skills.js
+++ src/Skills.js
@@ -6,60 +6,91 @@ export default function Skills() {
     <section className='skills' id='skills'>
       <h2 className='heading'>My <span className='sk'>Skills</span></h2>
 
-      <div className="skills-row">
-        <div className="skills-column">
+            <div className="skills-row">
+				<div className="skills-column">
+					<div className="skills-box">
+						<div className="skills-content">
+							<div className="progress">
+								<h3>
+									ReactJS <span>90%</span>
+								</h3>
+								<div className="bar">
+									<span></span>
+								</div>
+							</div>
 
-          <div className="skills-box">
-            <div className="skills-content">
-              <div className="progress">
-                <h3>HTML <span>90%</span></h3>
-                <div className="bar"><span></span></div>
-              </div>
+							<div className="progress">
+								<h3>
+									NodeJS <span>80%</span>
+								</h3>
+								<div className="bar">
+									<span></span>
+								</div>
+							</div>
 
-              <div className="progress">
-                <h3>CSS <span>80%</span></h3>
-                <div className="bar"><span></span></div>
-              </div>
+							<div className="progress">
+								<h3>
+									.NET <span>75%</span>
+								</h3>
+								<div className="bar">
+									<span></span>
+								</div>
+							</div>
 
-              <div className="progress">
-                <h3>Javascript <span>75%</span></h3>
-                <div className="bar"><span></span></div>
-              </div>
+							<div className="progress">
+								<h3>
+									Qlik Sense <span>80%</span>
+								</h3>
+								<div className="bar">
+									<span></span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
-              <div className="progress">
-                <h3>Java <span>80%</span></h3>
-                <div className="bar"><span></span></div>
-              </div>
-            </div>
-          </div>
-        </div>
+				<div className="skills-column1">
+					<div className="skills-box">
+						<div className="skills-content">
+							<div className="progress">
+								<h3>
+									SQL, MySQL Servers & Azure Databases <span>80%</span>
+								</h3>
+								<div className="bar">
+									<span></span>
+								</div>
+							</div>
 
-        <div className="skills-column1">
-          <div className="skills-box">
-            <div className="skills-content">
-              <div className="progress">
-                <h3>React <span>80%</span></h3>
-                <div className="bar"><span></span></div>
-              </div>
+							<div className="progress">
+								<h3>
+									Redux <span>80%</span>
+								</h3>
+								<div className="bar">
+									<span></span>
+								</div>
+							</div>
 
-              <div className="progress">
-                <h3>SQL <span>80%</span></h3>
-                <div className="bar"><span></span></div>
-              </div>
+							<div className="progress">
+								<h3>
+									PostGress & MongoDB <span>75%</span>
+								</h3>
+								<div className="bar">
+									<span></span>
+								</div>
+							</div>
 
-              <div className="progress">
-                <h3>Python <span>75%</span></h3>
-                <div className="bar"><span></span></div>
-              </div>
-
-              <div className="progress">
-                <h3>Oracle <span>80%</span></h3>
-                <div className="bar"><span></span></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+							<div className="progress">
+								<h3>
+									Java <span>80%</span>
+								</h3>
+								<div className="bar">
+									<span></span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
     </section>
   )
 }
